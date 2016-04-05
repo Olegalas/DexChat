eaf19f6dabab887ce13b38643b6fbc583b6ffbaa
olegalas <disposition@ukr.net>
2016-04-05 13:37:59 +0300

    override messaging logic. Use json

--- server/src/main/java/ua/dexchat/server/aop/PublicMethodsWrapper.java
+++ server/src/main/java/ua/dexchat/server/aop/PublicMethodsWrapper.java
@@ -11,41 +11,41 @@ import org.springframework.stereotype.Component;
 /**
  * Created by dexter on 04.04.16.
  */
-//@Aspect
-//@Component
+@Aspect
+@Component
 public class PublicMethodsWrapper {
 
-//    private static Logger LOGGER = Logger.getLogger(PublicMethodsWrapper.class);
-//
-//    @Before(value = "publicModelMethodsPointCut()")
-//    public void loggingPublicMethodsAdvice(){
-//        LOGGER.info("***public method of model was ended");
-//    }
-//
-//    @Pointcut(value = "execution(public * ua.dexchat.server..*(..))")
-//    public void publicServerMethodsPointCut(){}
-//
-//    @Pointcut(value = "execution(public * ua.dexchat.model..*(..))")
-//    public void publicModelMethodsPointCut(){}
-//
-//    @Around(value = "publicServerMethodsPointCut()")
-//    public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
-//
-//        String methodName = proceedingJoinPoint.getSignature().getName();
-//        LOGGER.info("***" + methodName + " method was started");
-//
-//        Object value = null;
-//
-//        try {
-//            value =  proceedingJoinPoint.proceed();
-//        } catch (Throwable throwable) {
-//            LOGGER.error("***" + methodName + " method threw exception");
-//            LOGGER.error(throwable.getMessage());
-//        }
-//
-//        LOGGER.info("***" + methodName + " method was ended");
-//
-//        return value;
-//    }
+    private static Logger LOGGER = Logger.getLogger(PublicMethodsWrapper.class);
+
+    @Before(value = "publicModelMethodsPointCut()")
+    public void loggingPublicMethodsAdvice(){
+        LOGGER.info("***public method of model was ended");
+    }
+
+    @Pointcut(value = "execution(public * ua.dexchat.server..*(..))")
+    public void publicServerMethodsPointCut(){}
+
+    @Pointcut(value = "execution(public * ua.dexchat.model..*(..))")
+    public void publicModelMethodsPointCut(){}
+
+    @Around(value = "publicServerMethodsPointCut()")
+    public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
+
+        String methodName = proceedingJoinPoint.getSignature().getName();
+        LOGGER.info("***" + methodName + " method was started");
+
+        Object value = null;
+
+        try {
+            value =  proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            LOGGER.error("***" + methodName + " method threw exception");
+            LOGGER.error(throwable.getMessage());
+        }
+
+        LOGGER.info("***" + methodName + " method was ended");
+
+        return value;
+    }
 
 }
